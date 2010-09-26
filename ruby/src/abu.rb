5abaa5502abd0414d76a2cee63fc2c7b3e0e821f
vinodkd <vinod@vinodkd.org>
2010-09-26 16:59:35 -0500

    Modified abu.rb and consequently its test file to: 1. Actually output java-ish text 2. Start working with the old hadoop syntax. This commit is to create a baseline before breaking the code to make it work with the 0.20 syntax.

--- ruby/src/abu.rb
+++ ruby/src/abu.rb
@@ -1,20 +1,34 @@
-#job job1:
-#	read 	(k1:type,v1:type) from "/path/to/file.ext" 	using DataReaderClassName
-#	map 	(k1:type,v1:type) to 	(k2:type,v2:type) 	using mapClassname 
-#	reduce 	(k2:type,v2:type) to 	(k2:type,v2:type) 	using redClassname
-#	write 	(k3:type,v3:type) to 	"/path/to/file.ext" 	using DataWriterClassName
-
 class Abu
-	def initialize(out)
-		@out=out
+	def initialize(outdir)
+		if File.exists? outdir and File.directory? outdir
+			@outdir=outdir
+		else
+			puts "Error: #{outdir} doesnt exist or isn't a directory"
+			exit
+		end
 	end
 	
-	def self.generate(out,&block)
-		new(out).instance_eval(&block)
+	def self.generate(outdir,&block)
+		abu = new(outdir)
+		abu.instance_eval(&block)
 	end
 	
 	def job(name)
-		@out << "\njob #{name}:\n"
+		@context=name
+		@jobout = File.new(File.join(@outdir,name.capitalize+'.java'),'w+')
+
+		# dont like the eval, but dont like the ugly here doc either.
+		# TODO: figure out the right way to do this
+		@jobout << eval('"'+ Templates::JOB_TEMPLATE_TOP + '"')
+		if block_given?
+			yield
+		end
+		@jobout << eval('"'+ Templates::JOB_TEMPLATE_BOT + '"')
+		nil
+	end
+
+	def mapreduce(name)
+		@context=name
 		if block_given?
 			yield
 		end
@@ -22,34 +36,85 @@ class Abu
 	end
 
 	def read(key, value, from, using)
-		@out << "read (#{key},#{value}) from #{from} using #{using}" << "\n"
+		@jobout << "read (#{key},#{value}) from #{from} using #{using}" << "\n"
 		nil
 	end
 
 	def map(key1, value1, key2, value2, using)
-		@out << "map (#{key1},#{value1}) to [(#{key2},#{value2})] using #{using}" << "\n"
+		mapout = File.new(File.join(@outdir,'Mapper.java'),'w+')
+		
+		# dont like the eval, but dont like the ugly here doc either.
+		# TODO: figure out the right way to do this
+		mapout << eval('"'+ Templates::MAP_TEMPLATE + '"')
 		nil
 	end
 
 	def reduce(key2, listOfvalue2, key3, value3, using)
-		@out << "reduce (#{key2},#{listOfvalue2}) to (#{key3},#{value3}) using #{using}" << "\n"
-		nil
-	end
+		reduceout = File.new(File.join(@outdir,'Reducer.java'),'w+')
 
-	def mapreduce(name)
-		@out << "\nmapreduce #{name}:\n"
-		if block_given?
-			yield
-		end
+		# dont like the eval, but dont like the ugly here doc either.
+		# TODO: figure out the right way to do this
+		reduceout << eval('"'+ Templates::REDUCE_TEMPLATE + '"')
 		nil
 	end
 
 	def exec(name,key1, value1, key3, value3)
-		@out << "#{name} (#{key1},#{value1}) to (#{key3},#{value3})\n"
+		@jobout << "#{name} (#{key1},#{value1}) to (#{key3},#{value3})\n"
 	end
 	
 	def write(key, value, to, using)
-		@out << "write (#{key},#{value}) to #{to} using #{using}" << "\n"
+		@jobout << "write (#{key},#{value}) to #{to} using #{using}" << "\n"
 		nil
-	end
+		end
+end
+
+module Templates
+	JOB_TEMPLATE_TOP=%q|
+public class #{@context.capitalize} {
+	public static void main(String[] args) throws Exception {
+
+		// your code goes here
+		Job job = new Job();
+		job.setJarByClass(#{@context.capitalize}.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+/*
+		job.setMapperClass(#{@context.capitalize}Mapper.class);
+		job.setReducerClass(NewMaxTemperatureReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+*/
+|
+
+	JOB_TEMPLATE_BOT=%q|
+	System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
+|
+
+	MAP_TEMPLATE=%q|
+import java.io.IOException;
+import java.io.InterruptedException;
+/*
+TODO: imports to be added
+*/
+static class #{@context.capitalize}Mapper extends Mapper<#{key1},#{value1},#{key2},#{value2}> {
+	public void map(#{key1} key, #{value1} value, Context context)	throws IOException, InterruptedException {
+		// your code goes here
+	}
+}
+|
+
+	REDUCE_TEMPLATE=%q|
+import java.io.IOException;
+import java.io.InterruptedException;
+/*
+TODO: imports to be added
+*/
+static class #{@context.capitalize}Reducer extends Reducer<#{key2},#{listOfvalue2},#{key3},#{value3}> {
+	public void reduce(#{key2} key, #{listOfvalue2} value, Context context)	throws IOException, InterruptedException {
+		// your code goes here
+	}
+}
+|
 end
--- ruby/test/abu_test.rb
+++ ruby/test/abu_test.rb
@@ -1,14 +1,14 @@
 require '../src/abu.rb'
 
-Abu.generate(STDOUT) do
-	job "job1" do
-		read "LongWritable","Text","/path/to/file.ext", "DataReaderClassName"
-		exec "mr1","LongWritable","Text","Text", "IntWritable"
-		write "Text", "IntWritable", "/path/to/file.ext", "DataWriterClassName"
+Abu.generate('./out') do
+	job 'job1' do
+		read 'LongWritable','Text','/path/to/file.ext', 'DataReaderClassName'
+		exec 'mr1','LongWritable','Text','Text', 'IntWritable'
+		write 'Text', 'IntWritable', '/path/to/file.ext', 'DataWriterClassName'
+	end
 
-		mapreduce "mr1" do
-			map "LongWritable","Text","Text", "IntWritable", "Mapper"
-			reduce "k2","v2", "k3","v3", "reducer"
-		end
+	mapreduce 'mr1' do
+		map 'LongWritable','Text','Text', 'IntWritable', 'Mapper'
+		reduce 'k2','v2', 'k3','v3', 'reducer'
 	end
 end
